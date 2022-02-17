de04d1726b4a6fdd594fa980956f17b302c9cc60
Nick <92036651+dpotomahin@users.noreply.github.com>
2022-02-17 21:03:44 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+// Function 1
+// function display_massage(){
+//     alert('Это мое первое сообщение!!!');
+//     console.log('Это мое первое сообщение!!!');
+// }
+// display_massage();
+
+
+// Function 2
+// function favorite_books(){
+//     title = prompt('What is your favorite books?');
+//     alert(`One is my favorite books is ${title}`);
+//     console.log(`One is my favorite books is ${title}`);
+// }
+// favorite_books()
+
+
+// Function 3
+// function make_shirt(){
+//     s = prompt('What is your T-shirt size');
+//     t = prompt('What caption would you like to add?');
+//     alert(`Your size is ${s}, and caption would you like to add is ${t}`);
+//     console.log(`Your size is "${s}", and caption would you like to add is "${t}"`);
+// }
+// make_shirt();
+
+
+// Function 4
+// s = prompt('What is your T-shirt size');
+// t = prompt('What caption would you like to add?');
+// function make_shirt(size, text, w){
+//     alert(`${w} size is ${size}, and caption to add is ${text}`);
+//     console.log(`${w} size is "${s}", and caption would  is "${t}"`);
+// }
+// make_shirt("L", '"I love Python"', "Standart T-shirt");
+// make_shirt(s, t, "Your")
+
+
+// Function 5
+// function describe_city(){
+//     c = prompt('Enter the name of any city');
+//     alert(`${c} is in America`);
+//     console.log(`${c} is in America`);
+// }
+// describe_city();
